849afe57edef181d1dda1a4ee2c8d78b65236270
Iftekher Sunny <iftekhersunny@hotmail.com>
2016-12-25 16:56:49 +0600

    Removed tests route

--- src/configs/app.go
+++ src/configs/app.go
@@ -2,6 +2,6 @@ package configs
 
 // app configuration variables
 const (
-	MAILSCOUT_WEBHOOK_LINK = "http://localhost:8000/tests/reports/"
+	MAILSCOUT_WEBHOOK_LINK = "http://localhost/reports/"
 	SHORT_URL_STRING_SIZE  = 8
 )
